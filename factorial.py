aea4db402d2a44c38f83e643af0dbcdb8d5811eb
hphadke29 <34576498+hphadke29@users.noreply.github.com>
2019-05-16 17:32:22 +0100

    Add files via upload
    adding a new test file

---
+++ factorial.py
@@ -0,0 +1,52 @@
+from __future__ import division
+import sys
+
+# Place holder for test fucntion for CIF
+
+
+def fact(n):
+    """
+    Factorial function
+
+    :arg n: Number
+    :returns: factorial of n
+
+    """
+    if n == 0:
+        return 1
+    return n * fact(n - 1)
+
+
+def div(n):
+    """
+	Simple Divide Function
+	"""
+    result = 33 / n
+    return round(result, 2)
+
+
+def stest_sum(n):
+    """
+	It takes an iterable and adds the values together
+	"""
+    total = 0
+    for val in n:
+        total += val
+    return total
+
+
+def main(n, y):
+    """
+	Main function
+	"""
+    res = fact(n)
+    print(res)
+    res1 = div(y)
+    print(res1)
+    res2 = stest_sum([n, y])
+    print(res2)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        main(int(sys.argv[1]), int(sys.argv[2]))
